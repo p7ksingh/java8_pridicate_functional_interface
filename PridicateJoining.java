48e5d99a5b4894f0cb9bf2c8d6041a278396d695
Developer <developer@Developers-Mac-mini.local>
2023-12-20 20:27:36 +0530

    adding  example

---
+++ PridicateJoining.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PridicateJoining {
+
+    public static void main(String[] args) {
+        // System.out.println("negate(),and(),or()");
+        Predicate<Integer> p = dt -> dt > 5;
+        System.out.println(p.test(11));
+
+        Predicate<String> p1 = dt -> dt.length() > 10;
+      //  Predicate<String> p2 = dt -> dt.equalsIgnoreCase("pankajkumarsingh");
+        Predicate<String> p3 = p1.and(p2);
+      //  System.out.println(p3.test("Pankajkumarsingh"));
+        Predicate<Collection> p4 = dt -> dt.isEmpty();
+        List list = new ArrayList();
+        System.out.println(p4.test(list));
+        // filter the data with >10 and even number
+        int[] x = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+        Predicate<Integer> p5 = dt -> dt > 10;
+        Predicate<Integer> p6 = dt -> dt % 2 == 0;
+        Predicate<Integer> p7 = p5.and(p6);
+        m1(p7, x);
+    }
+
+    public static void m1(Predicate<Integer> p, int[] x) {
+        for (int x1 : x) {
+            if (p.test(x1)) {
+                System.out.println(x1);
+            }
+        }
+    }
+}
